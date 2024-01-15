dd84091b0111e1780ac094b323e875056815e887
khalil baba <33592578+khalybangs@users.noreply.github.com>
2024-01-15 13:00:01 +0100

    Update noti.js

--- nodes/noti.js
+++ nodes/noti.js
@@ -7,7 +7,7 @@ const cltusers = "users";
 const session = require('express-session');
 
 const cors = require('cors');
-var orgns = ["http://localhost:5000", "http://localhost:3000", "http://192.168.247.239:3000"]
+var orgns = "*";
 notinodes.use(cors({
     origin: orgns
 }));
